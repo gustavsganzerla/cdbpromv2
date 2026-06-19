77a7f54701b787ffeed137090aa385b33801caeb
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2026-06-19 09:23:29 -0300

    home url

--- django/myapp/urls.py
+++ django/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("query/", views.query, name="query"),
     path("group_summary_page/", views.group_summary_page, name="group_summary_page"),
     path("predict/", views.predict, name="predict"),
